d013d9aa2efaf8e8a1ea16891fb6ab53de0b3911
Jonas Peterburs <j.peterburs@protonmail.com>
2025-01-02 16:24:07 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <article>
+    <span>{{ result }}</span>
+
+    <button v-on:click="rollD20()" type="button">Reroll</button>
+  </article>
+</template>
+
+<style scoped>
+span {
+  font-size: 9rem;
+  display: grid;
+  place-content: center center;
+}
+
+button {
+  width: 100%;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      result: null
+    }
+  },
+  methods: {
+    rollD20() {
+      this.result = Math.floor(Math.random() * 20) + 1
+    }
+  },
+  mounted() {
+    this.rollD20()
+  }
+}
+</script>
